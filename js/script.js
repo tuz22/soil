001e607165535d6dfb978022bcc97ed774c2bc6f
tuz22 <seungji.dev@gmail.com>
2022-08-25 21:44:42 +0900

    아이콘 추가, 기록 페이지 추가, 카테고리 탭

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function btnPlus(e) {
   e.classList.toggle("change");
 }
+function openCategories(url){
+  window.open(url,'','location=no, scrollbars=no, width = 500, height = 500');
+}
+function closeCategories(e) {
+  let arr = new Array()
+  arr = document.getElementsByTagName("button");
+  let value;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i].click) {
+      value = arr[i].value;
+      break;
+    }
+  }
+  window.close();
+  window.opener.document.getElementById(e).value = value;
+}
