98e8c854da2675de6489e5c2d34167dcc32f553b
tuz22 <seungji.dev@gmail.com>
2022-08-27 18:35:13 +0900

    board_write 페이지, setting 페이지 추가

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function btnPlus(e) {
   e.classList.toggle("change");
 }
+
 function openCategories(url){
   window.open(url,'','location=no, scrollbars=no, width = 500, height = 500');
 }
+
 function closeCategories(e) {
   let arr = new Array()
   arr = document.getElementsByTagName("button");
@@ -16,4 +18,30 @@ function closeCategories(e) {
   }
   window.close();
   window.opener.document.getElementById(e).value = value;
+}
+
+function count(type) {
+  const resultElement = document.getElementById('btnFontSize');
+  let fontSize = resultElement.innerText;
+
+  if (type === 'plus') { 
+    fontSize = parseInt(fontSize) + 1;
+  } else if (type === 'minus') {
+    fontSize = parseInt(fontSize) - 1;
+  }
+
+  resultElement.innerText = fontSize;
+}
+
+function fontStyle(type) {
+  const resultElement = document.getElementById('btnFontChange');
+  let fontStyle = resultElement.innerText;
+
+    if (type === 'plus') { 
+      fontStyle = "font2";
+    } else if (type === 'minus') {
+      fontStyle = "Gaegu";
+    }
+  
+  resultElement.innerText = fontStyle;
 }
