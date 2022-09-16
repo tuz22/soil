d5dda6882cd88c4e777bfc0ab938d0ca68b63298
Seungji <96293047+tuz22@users.noreply.github.com>
2022-09-17 01:57:19 +0900

    새 diary 등록

--- js/diary.js
+++ js/diary.js
@@ -4,13 +4,8 @@
 //   }, 300);
 // })
 
-// 회원 일기 목록 조회
-$(document).ready(function() {
-  diaryIndex();
-  // console.log(sessionStorage.getItem('loginMember'));
-});
-  
-  function diaryIndex(){
+// 회원 일기 목록 조회  
+  function diaryIndex() {
   $.ajax({
     type: "GET",
     url: "http://15.165.102.73:8090/diaries/5",
@@ -48,3 +43,70 @@ $(document).ready(function() {
     }
   });
 };
+
+$(document).ready(function() {
+  diaryIndex();
+  // console.log(sessionStorage.getItem('loginMember'));
+});
+
+/* 현재 날짜 */
+let today = new Date();
+
+let year = today.getFullYear();
+let month = ('0' + (today.getMonth() + 1)).slice(-2);
+let day = ('0' + today.getDate()).slice(-2);
+let date = year + '-' + month + '-' + day;
+
+document.getElementsByClassName('date')[0].innerHTML = date;
+
+/* 새 일기 등록 */
+function diaryRegister() {
+  let diaryCategory = document.getElementById('diaryCategory').value;
+  let diaryTitle = document.getElementById('diaryTitle').value;
+  let diaryPrice = document.getElementById('diaryPrice').value;
+  let diaryContent = document.getElementById('diaryContent').value;
+  
+  let userId = 'memberA';
+  
+  let newDiary = {"member_id": userId, "category_id": diaryCategory, "title": diaryTitle, "content": diaryContent, "price": diaryPrice};
+
+  $.ajax({
+    type: "POST",
+    url: "http://15.165.102.73:8090/diaries",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(newDiary),
+    secure: true,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest"
+    },
+    success: function(result) {
+      if (result == -1) {
+        alert('실패');
+        return false;
+      } else {
+        alert('성공');
+        alert(msg);
+      }
+    },
+    error: function(error) {
+      console.log(error);
+      alert('에러!');
+    }
+  })
+}
+
+document.getElementsByClassName('history-back')[0].addEventListener('click', function(){
+  let diaryCategory = document.getElementById('diaryCategory').value;
+  let diaryTitle = document.getElementById('diaryTitle').value;
+  let diaryPrice = document.getElementById('diaryPrice').value;
+  let diaryContent = document.getElementById('diaryContent').value;
+  
+  alert(diaryCategory + diaryTitle + diaryPrice + diaryContent);
+  if (diaryTitle != "" && diaryPrice != "" && diaryContent != "") {
+    alert('일기 저장할거임');
+    diaryRegister();
+  } else {
+    alert('일기 저장안함');
+  }
+})
