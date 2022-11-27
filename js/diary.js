b3a5e4fd2b5da7f98266cb46e681043bacbfbb04
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-27 15:55:17 +0900

    회원 일기 추가 조회 수정 : 더보기 버튼

--- js/diary.js
+++ js/diary.js
@@ -1,11 +1,16 @@
-let api_key = localStorage.getItem('api_key');
-console.log(api_key)
+const api_key = localStorage.getItem('api_key');
+const diaryTable = document.getElementsByClassName('diary');
+const diaryIds = document.getElementsByClassName('diary-id');
+const addBtn = document.querySelector('.add-btn');
+const numb = 0;
 
 // 회원 일기 목록 조회  
-  function diaryIndex(numb) {
+function diaryIndex() {
+console.log('목록조회!')
+    console.log('numb = '+ numb)
   $.ajax({
     type: "GET",
-    url: "http://15.165.102.73:8090/api/diaries/list?limit=10&offset="+numb,
+    url: "http://15.165.102.73:8090/api/diaries/list?limit=3&offset="+ numb,
     dataType: "json",
     cors: true,
     contentType: "application/json",
@@ -19,14 +24,14 @@ console.log(api_key)
 
       let diaryData = data.response.diaryList;
       console.log(diaryData)
-      console.log(diaryData[1].id)
+      // console.log(diaryData[1].id)
       let str = "";
       $.each(diaryData, function(i){
         // str += "<table class='diary' onclick='location.href=`diary_read.html`'>"
         str += "<table class='diary'>"
         str += "  <thead>"
         str += "    <tr>"
-        str += "      <td id='diaryNumb'>" + (i+1)
+        str += "      <td id='diaryNumb'>" + diaryData[i].category.name
         str += "        <input class='diary-id' value='" + diaryData[i].id + "'>"+ "</td><td>|</td>"
         str += "    </tr>"
         str += "  </thead>"
@@ -40,6 +45,7 @@ console.log(api_key)
         str += "</a>"
       });
       $('#tableDiary').append(str);
+      numb = numb + 3;
     },
     error: function(error){
       console.log(error);
@@ -81,22 +87,47 @@ function readDiary(diaryId) {
 }
 
 setTimeout(() => {
-  let diaryTable = document.getElementsByClassName('diary');
-  let diaryIds = document.getElementsByClassName('diary-id');
   
-  for (let i = 0; i < diaryTable.length; i++ ) {
-    diaryTable[i].addEventListener('click', function(e){
-      document.querySelector('.diary-index').classList.add('off');
-      document.querySelector('.diary-read').classList.remove('off');
-      document.querySelector('footer').style.display = 'none'
-      console.log(e.currentTarget)
-      let diaryIdValue = diaryIds[i].value;
-      console.log("id: "+diaryIdValue);
-      readDiary(diaryIdValue);
-    });
+  goReadDiary();
+  console.log('처음에만 되나?')
+  console.log(diaryTable.length);
+}, 100);
+
+function goReadDiary(){
+  console.log(diaryTable.length);
+  
+  setTimeout(() => {
+    console.log('setTimeout!')
+    for (let i = 0; i < diaryTable.length; i++ ) {
+    
+      diaryTable[i].addEventListener('click', function(e){
+        document.querySelector('.diary-index').classList.add('off');
+        document.querySelector('.diary-read').classList.remove('off');
+        document.querySelector('footer').style.display = 'none'
+        console.log(e.currentTarget)
+        let diaryIdValue = diaryIds[i].value;
+        console.log("id: "+diaryIdValue);
+        readDiary(diaryIdValue);
+      });
     }
-}, 1000);
+    console.log('반복문 다 돌았음!')
 
-/* 일기 더보기 버튼 */
-let diaryTable = document.getElementsByClassName('diary');
+  }, 100);
+}
 
+/* 회원 일기 추가 조회 : add 버튼 */
+
+async function clickAddBtn(){
+  const addBtnPromise = new Promise(function(success){
+    
+    addBtn.addEventListener('click', function(){
+      success(goReadDiary());
+      console.log(diaryTable.length);
+      console.log('됏다!');
+    })
+  
+  });
+  await addBtnPromise;
+  console.log(diaryTable.length);
+}
+clickAddBtn();
