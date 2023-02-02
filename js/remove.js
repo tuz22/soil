4ad05b93c0494cf2ff0f0f0552479170f0b9d047
Seungji <96293047+tuz22@users.noreply.github.com>
2023-02-02 22:48:17 +0900

    회원탈퇴 추가

---
+++ js/remove.js
@@ -0,0 +1,34 @@
+/* 회원탈퇴 */
+function removeUser() {
+  $.ajax({
+    type: "DELETE",
+    url: "https://www.soildiary.net/api/users/remove",
+    dataType: "json",
+    contentType: "application/json",
+    cors: true,
+    secure: true,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+      "api_key" : api_key
+    },
+    success: function() {
+      console.log('회원 탈퇴 성공');
+      location.href = "index.html";
+    },
+    error: function(error) {
+      console.log(error);
+      alert('에러!');
+    }
+  })
+}
+
+document.getElementById('removeUser').addEventListener('click', function(){
+  console.log('클릭되나')
+  const deleteMsg = confirm('해당 앱을 탈퇴하시겠습니까?');
+
+  if(deleteMsg == true){
+    removeUser();
+  } else {
+    alert('탈퇴 취소');
+  }
+});
