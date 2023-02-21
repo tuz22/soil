cae7376db19ce36af1d8290c1564c8ba2ddaaaee
tuz22 <seungji.dev@gmail.com>
2023-02-22 00:14:57 +0900

    카테고리 등록

--- js/categories.js
+++ js/categories.js
@@ -7,8 +7,37 @@ function categoryList() {
     type: "GET",
     url: "https://www.soildiary.net/api/category/categoryList",
     dataType: "json",
-    cors: true,
     contentType: "application/json",
+    cors: true,
+    secure: true,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+      "api_key" : api_key,
+    },
+    success: function(data) {
+      console.log(data)
+      console.log(JSON.stringify(data))
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+
+/* 카테고리 등록 */
+function createCategory() {
+  const categoryInput = document.getElementById('categoryInput').value
+  console.log(categoryInput)
+  const categoryName = {"categoryName" : categoryInput};
+  
+  $.ajax({
+    type: "POST",
+    url: "https://www.soildiary.net/api/category/add",
+    dataType: "json",
+    contentType: "application/json",
+    data: categoryName,
+    cors: true,
     secure: true,
     headers: {
       "X-Requested-With": "XMLHttpRequest",
