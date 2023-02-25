1be27c5d606356c7114733fc1a55fc66d0f7f1d0
tuz22 <seungji.dev@gmail.com>
2023-02-26 01:02:48 +0900

    json data로 수정

--- js/categories.js
+++ js/categories.js
@@ -28,15 +28,44 @@ function categoryList() {
 /* 카테고리 등록 */
 function createCategory() {
   const categoryInput = document.getElementById('categoryInput').value
-  console.log(categoryInput)
   const categoryName = {"categoryName" : categoryInput};
+  console.log(categoryInput)
+  console.log(categoryName)
 
   $.ajax({
     type: "POST",
     url: "https://www.soildiary.net/api/category/add",
     dataType: "json",
     contentType: "application/json",
-    data: categoryName,
+    data: JSON.stringify(categoryName),
+    cors: true,
+    secure: true,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+      "api_key" : api_key,
+    },
+    success: function(data) {
+      console.log(data)
+      console.log(JSON.stringify(data))
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+/* 카테고리 수정 */
+function updateCategory() {
+  const categoryInput = document.getElementById('categoryInput').value
+  console.log(categoryInput)
+  const categoryData = {"newName": "string","originName": "string"};
+
+  $.ajax({
+    type: "POST",
+    url: "https://www.soildiary.net/api/category/update",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(categoryData),
     cors: true,
     secure: true,
     headers: {
