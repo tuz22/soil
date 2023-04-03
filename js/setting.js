98332f5b3c18900f209458e5ea2898faee97289a
tuz22 <seungji.dev@gmail.com>
2023-04-04 01:57:28 +0900

    설정 수정

--- js/setting.js
+++ js/setting.js
@@ -3,47 +3,47 @@
 // }
 
 // for (let i= 0; i <2; i++) {
-  document.getElementsByClassName('set-darkmode')[0].addEventListener('click', function(e){
-    const btn = e.currentTarget;
-    if (btn.innerHTML == 'OFF') {
-      btn.innerHTML = 'ON'
-    } else {
-      btn.innerHTML = 'OFF'
-    }
-    btn.classList.toggle('set-btn-active')
-  })
+//   document.getElementsByClassName('set-darkmode')[0].addEventListener('click', function(e){
+//     const btn = e.currentTarget;
+//     if (btn.innerHTML == 'OFF') {
+//       btn.innerHTML = 'ON'
+//     } else {
+//       btn.innerHTML = 'OFF'
+//     }
+//     btn.classList.toggle('set-btn-active')
+//   })
+// // }
+
+// function fontStyle(type) {
+//   const resultElement = document.getElementById('btnFontChange');
+//   let fontStyle = resultElement.innerText;
+  
+//   if (type === 'plus') { 
+//     fontStyle = "font2";
+//   } else if (type === 'minus') {
+//     fontStyle = "Gaegu";
+//   }
+  
+//   resultElement.innerText = fontStyle;
 // }
 
-function fontStyle(type) {
-  const resultElement = document.getElementById('btnFontChange');
-  let fontStyle = resultElement.innerText;
+// // 글자 크기 조절
+// function fontSize(){
   
-  if (type === 'plus') { 
-    fontStyle = "font2";
-  } else if (type === 'minus') {
-    fontStyle = "Gaegu";
-  }
-  
-  resultElement.innerText = fontStyle;
-}
+// }
 
-// 글자 크기 조절
-function fontSize(){
-  
-}
+// function count(type) {
+//   const resultElement = document.getElementById('btnFontSize');
+//   let fontSize = resultElement.innerText;
 
-function count(type) {
-  const resultElement = document.getElementById('btnFontSize');
-  let fontSize = resultElement.innerText;
+//   if (type === 'plus') { 
+//     fontSize = parseInt(fontSize) + 4;
+//   } else if (type === 'minus') {
+//     fontSize = parseInt(fontSize) - 4;
+//   }
 
-  if (type === 'plus') { 
-    fontSize = parseInt(fontSize) + 4;
-  } else if (type === 'minus') {
-    fontSize = parseInt(fontSize) - 4;
-  }
-
-  resultElement.innerText = fontSize;
-}
+//   resultElement.innerText = fontSize;
+// }
 
 // 카테고리 설정으로 이동
 function goCategories(){
